e13862f434bbb343d9082a9e0c5c2e8787e9715c
Stephen Crawley <s.crawley@uq.edu.au>
2014-05-26 14:14:58 +1000

    Fix ...

--- libraries/zone_info.rb
+++ libraries/zone_info.rb
@@ -62,10 +62,7 @@ module ZoneInfo
   # an IP address.  Return nil if the IP is not in a known NeCTAR zone.
   def ip2ZoneRange(ip) 
     ZONE_RANGES.each() do |zi|
-      puts "ip = #{ip}, start = #{zi['start']}, end = #{zi['end']}"
-      puts "#{zi['start'] <=> ip}, #{zi['end'] <=> ip}"
-      if (zi['start'] <=> ip) >= 0 and (zi['end'] <=> ip) <= 0 then
-        puts 'Hit!'
+      if (zi['start'] <=> ip) <= 0 and (zi['end'] <=> ip) >= 0 then
         return zi
       end
     end
